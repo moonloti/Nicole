1fe72c0503dd4a3e0624e431a9ef0606d3d6aa3a
Nicole <moonloti_nicole07@berkeley.edu>
2026-02-11 23:34:42 -0800

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,72 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+    # Git is the engine for version control, Git Bash allows Git commands to run locally while Git Hub hots and collaborates on resporitories online.
+
+# 2) What’s the difference between the terminal and the command line?
+    # The terminal line is the window you open to interact with your computer using test while the command line is the actual program inside the terminal that processes your commands.
+
+# 3) How does Windows PowerShell differ from Git Bash?
+    # Windows Powershell is more powerful for system management while Git Bash is common for coding and Git usage
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+    # Python is the programming langugae, Conda a package and environment manager while Anaconda is a distribution that includes Python, conda and many more.
+
+# 5) What is VS Code? 
+    # VS Code allows us to run the python extension and allow us to work with python
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+    # A Jupyter Notebook is an application where you can write and run code in small sections while the JupyterLab is more advanced and includes the Jupyter Notebook features and much more
+
+# 7) What does ~/ mean?
+    # ~/ means the home directory in Git Bash
+
+# 8) What’s the difference between an absolute path and a relative path?
+    # The absolute path is the full address from the root, independent of where you are while the relative path is the location based on your current working directory.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+    # Relative path: course_assignments/homework2
+    # Absolute path: C:/Users/yourname/course_assignments/homework2
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+    # cd ..
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+    # rm ./ would delete all files and folders in your current directory.
+
+# 12) What do the following commands do?
+# git add
+    # adds changes to the staging area so they are ready to be committed
+# git commit
+    # saves the stages changes to the local repository wth a message
+# git push
+    # uploads your commited changes from your local repository to a remote repository
+    # The following commands allow you to save your work.
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+    # "git add ." adds all the changed files in the current directory to staging while "git add <file>" adds only the specific file you name to staging
+
+# 14) What do "git status" and "git log -1" do?
+    # "git status" shows the current state of your repository while "git log -1" shows information about the most recent commit only
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+    # Cloning a repository is the process of creating a local copy of a remote repository while pulling from a repository updates your local copy with the latest changes from the remote repository.
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+    # So far I had a significant amount of issues with installations and trying to keep up with the class even though I was having trouble. However, I quickly resolved this issue by going through the process from scratch and starting over, even turning to Google for some parts.
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+    # I understand the commands but I am a little unsure about how everything (Git, GitHub, terminal, and repositories) connects together a real workflow.
+
+# 18) Tell me a fun fact!
+    # Fun fact: I am a twin :)
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(10 % 3)
+# % is a operator that gives the remainder after division so 10 divided by 3 is 9 but 10-9 is 1 so there is a remainder of one left over.
